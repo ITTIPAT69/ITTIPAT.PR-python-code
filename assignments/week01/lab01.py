3362166195f5376947beecad212987ca7fd8705c
Ittipat Primprai <ittipat.pr@ku.th>
2025-07-08 12:14:22 +0700

    comment

--- assignments/week01/lab01.py
+++ assignments/week01/lab01.py
@@ -1,1 +1,13 @@
-print("test")
+# Write your solution here:
+name = input("What is your name? :")
+age = input("How old are you? :")
+email = input("What is your email? :")
+phone = input("What is your phone number? :")
+hobby = input("What is your favorite hobby? :")
+
+print("== Your profile ==")
+print("Name:",name)
+print("Age:",age)
+print("Email:",email)
+print("Phone Number:",phone)
+print("Favorite Hobby",hobby)
